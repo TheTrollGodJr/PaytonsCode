41967a61b7f91778a3d549f7d0f37cb01a4b6d73
TheTrollGodJr <69094231+TheTrollGodJr@users.noreply.github.com>
2023-05-07 19:38:17 -0700

    Update load-save.py

--- load-save.py
+++ load-save.py
@@ -1,43 +1,54 @@
+#bool variables you want to save
 boolA = False
 boolB = True
 boolC = True
 
+#create a string with each bool in it
 save = [boolA, boolB, boolC]
 
+
+#SAVE INFORMATION:
+
+#this creates a text file name "save.txt". The "w" means that you are writing to it
 f = open("save.txt", "w")
-f.write("")
-f.close()
+f.write("") #this is creating the file/clearing it if it already exists
+f.close() #closes the file
 
-f = open("save.txt", "a")
-for i in range(len(save)):
-    f.write(str(save[i]) + ",")
-    print(str(save[i]))
+f = open("save.txt", "a") #opens the "save.txt" file, the "a" means that you are appending to it
+for i in range(len(save)): #for loop going through the save list
+    f.write(str(save[i]) + ",") #systematically saving each value in the save list to "save.txt" with a comma after it
 
-f.close()
-print("\n\n")
+f.close() #close the file
 
-f = open("save.txt", "r")
-line = f.readline()
-f.close()
 
-save = line.split(",")
-print(save)
 
+#LOAD INFORMATION:
+
+f = open("save.txt", "r") #opens "save.txt", the "r" means that you are reading the file
+line = f.readline() #saves the first line of "save.txt" as a variable
+f.close() #close the file
+
+save = line.split(",") #.split() save the line we loaded from "save.txt" into the save variable but splits it at each comma; 1,2,3 would become ["1","2","3"]
+
+#sets the variable true or false
 if save[0] == "True":
     boolA = True
 else:
     boolA = False
 
+#sets the variable true or false
 if save[1] == "True":
     boolB = True
 else:
     boolB = False
 
+#sets the variable true or false
 if save[2] == "True":
     boolC = True
 else:
     boolC = False
 
+#prints the bool values
 print(str(boolA))
 print(str(boolB))
 print(str(boolC))
